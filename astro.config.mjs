af889cbc65c3ebc8997a6badd14efbcf5f6818b2
Tu Nombre <angelpaqui36@gmail.com>
2026-08-12 14:23:22 -0400

    Fix GitHub pages sub-directory path resolving by explicitly prepending /barberia-demo/ to image urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://apaquig.github.io',
-  base: '/barberia-demo',
+  base: '/barberia-demo/',
 });
